dae08c792980e02cad5a8c5f3a081f62af2b2a4d
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-04-02 10:16:03 -0300

    Implementa funcionalidade de lixeira

--- index.js
+++ index.js
@@ -18,10 +18,20 @@ const listaLembretesRenderizados = document.getElementById(
 );
 
 arrayLembretes.lembretes.forEach((lembrete) => {
+  let container = document.createElement("div");
+  container.classList.add("lembrete-container");
+
   let li = document.createElement("li");
   li.innerHTML = `A atividade: <strong>${lembrete.atividade}</strong> será notificada em: <strong>${lembrete.tempoMinuto} minutos</strong>`;
 
-  listaLembretesRenderizados.appendChild(li);
+  let icon = document.createElement("button");
+  icon.innerHTML = '<i class="fa-solid fa-trash"></i>';
+  icon.classList.add("icon-lembrete");
+
+  container.appendChild(li);
+  container.appendChild(icon);
+
+  listaLembretesRenderizados.appendChild(container);
 });
 
 document
@@ -32,7 +42,7 @@ document
     const nomeLembrete = document.getElementById("nomeInput").value;
     const tempoLembrete = document.getElementById("tempoLembrete").value;
 
-    if (!nomeLembrete || !tempoLembrete) return;
+    if (!nomeLembrete.trim() || !tempoLembrete.trim()) return;
 
     let novoLembrete = {
       id: arrayLembretes.lembretes.length + 1,
@@ -43,14 +53,33 @@ document
 
     arrayLembretes.lembretes.push(novoLembrete);
 
+    let container = document.createElement("div");
+    container.classList.add("lembrete-container");
+
     let li = document.createElement("li");
     li.innerHTML = `A atividade: <strong>${novoLembrete.atividade}</strong> será notificada em: <strong>${novoLembrete.tempoMinuto} minutos</strong>`;
-    document.getElementById("lembreteAddRenderizado").appendChild(li);
+
+    let icon = document.createElement("button");
+    icon.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    icon.classList.add("icon-lembrete");
+
+    icon.addEventListener("click", () => {
+      container.remove();
+      arrayLembretes.lembretes = arrayLembretes.lembretes.filter(
+        (lembrete) => lembrete.id !== novoLembrete.id
+      );
+    });
+
+    container.appendChild(li);
+    container.appendChild(icon);
+    document.getElementById("lembreteAddRenderizado").appendChild(container);
 
     document.getElementById("nomeInput").value = "";
     document.getElementById("tempoLembrete").value = "";
 
-    const tmpMilisegundo = arrayLembretes.lembretes.tempoMinuto * 60000;
-
-    alertaLembrete(() => {}, tmpMilisegundo);
+    const tmpMilisegundo = novoLembrete.tempoMinuto * 60000;
+    alertaLembrete(() => {
+      const reminder = document.getElementById("reminder");
+      reminder.innerText = "Lembretes Ativos:";
+    }, tmpMilisegundo);
   });
